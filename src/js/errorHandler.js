78e52ffd395840476d0f92a278af1adcebc7b32e
Otavio <otavio.zerotwo@gmail.com>
2026-08-16 13:31:54 -0300

    feat: add fluxo de validação campos do formulário

---
+++ src/js/errorHandler.js
@@ -0,0 +1,53 @@
+const inputError = (element, elementContainer) => {
+  const spanErrorMessage = elementContainer.querySelector('.error-message');
+
+  if (element.value === '' || element.value == false) {
+    elementContainer.classList.add('invalid-field');
+    spanErrorMessage.style.display = 'block';
+
+    return false;
+  } else {
+    spanErrorMessage.style.display = 'none';
+    elementContainer.classList.remove('invalid-field');
+
+    return true;
+  }
+}
+
+const radioError = (element, elementContainer) => {
+  const spanErrorMessage = elementContainer.querySelector('.error-message');
+  
+  if (!element) {
+    elementContainer.classList.add('invalid-field');
+    spanErrorMessage.style.display = 'block';
+
+    return false;
+  } else {
+    spanErrorMessage.style.display = 'none';
+    elementContainer.classList.remove('invalid-field');
+
+    return true;
+  }
+}
+
+const checkboxError = (element, elementContainer) => {
+  const spanErrorMessage = elementContainer.querySelector('.error-message');
+  
+  if (!element) {
+    elementContainer.classList.add('invalid-field');
+    spanErrorMessage.style.display = 'block';
+
+    return false;
+  } else {
+    spanErrorMessage.style.display = 'none';
+    elementContainer.classList.remove('invalid-field');
+
+    return true;
+  }
+}
+
+export {
+  inputError,
+  radioError,
+  checkboxError
+}
---
+++ src/js/main.js
@@ -0,0 +1,47 @@
+import { inputError, radioError, checkboxError } from './errorHandler';
+
+const form = document.getElementById('form');
+const inputGroupFirstName = document.getElementById('inputGroupFirstName');
+const firstName = document.getElementById('firstName');
+const inputGroupLastName = document.getElementById('inputGroupLastName');
+const lastName = document.getElementById('lastName');
+const inputGroupEmail = document.getElementById('inputGroupEmail');
+const email = document.getElementById('email');
+const inputGroupMessage = document.getElementById('inputGroupMessage');
+const message = document.getElementById('message');
+const inputGroupQueryType = document.getElementById('inputGroupQueryType');
+const checkboxGroup = document.getElementById('checkboxGroup');
+
+const validateForm = () => {
+  const queryType = document.querySelector('input[name="queryType"]:checked');
+  const checkConsent = document.getElementById('checkConsent').checked;
+
+  const isFirstNameValid = inputError(firstName, inputGroupFirstName);
+  const isLastNameValid = inputError(lastName, inputGroupLastName);
+  const isEmailValid = inputError(email, inputGroupEmail);
+  const isQueryTypeValid = radioError(queryType, inputGroupQueryType);
+  const isMessageValid = inputError(message, inputGroupMessage);
+  const isCheckConsentValid = checkboxError(checkConsent, checkboxGroup);
+
+  if (
+    isFirstNameValid &&
+    isLastNameValid &&
+    isEmailValid &&
+    isQueryTypeValid &&
+    isMessageValid &&
+    isCheckConsentValid
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const isFormValid = validateForm();
+
+  isFormValid && form.reset();
+});
+
